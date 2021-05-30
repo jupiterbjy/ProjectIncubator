1696c471947c2b10bf78edf4b82150be28dea347
jupiterbjy <jupiterbjy@gmail.com>
2021-05-31 00:18:00 +0900

    back-porting from private repository.

--- Meow_py/__main__.py
+++ Meow_py/__main__.py
@@ -1,28 +1,25 @@
-import logging
+#!/usr/bin/python3
+
 import argparse
 import asyncio
-import itertools
-
+import pathlib
 
 from discord.ext import commands
-from discord import DiscordException
-from discord.errors import HTTPException
+from discord import DiscordException, Embed, Game
+from loguru import logger
+from traceback_with_variables import activate_by_import
 
 
-from log_initalizer import init_logger
-
-
-bot = commands.Bot(command_prefix="/")
+assert activate_by_import
 
 
 end_signature = "\u200a\u200a\u200a"
 end_signature_encoded = end_signature.encode("utf8")
-results_format = "Run results:\n\n{}"
-
-codec = "utf8"
-code_format = "python3 <<EOF\n{}\nEOF"
-msg_format = "```\n{}\n```"
-overflow_msg = "..."
+record_path = (
+    pathlib.Path(__file__)
+    .parent.joinpath("YoutubeStreamStatLogger/Records/Cyan Nyan/")
+    .absolute()
+)
 
 
 def encode(string: str):
@@ -37,84 +34,158 @@ def decode(byte_string: bytes):
     return decoded.removeprefix(end_signature)
 
 
-@bot.event
-async def on_ready():
-    print(f"{bot.user} connected.")
+def assign_actions(bot: commands.bot):
 
-    if not any(guild.id == args.guild_id for guild in bot.guilds):
-        logger.critical("Bot is not connected to given server ID %s", args.guild_id)
+    first_setup_done = False
 
-        raise DiscordException(f"Bot is not connected to given server ID {args.guild_id}")
+    async def first_call():
 
+        message = "Meow World, Nyanstaree~🌟 I am a simple python bot you can play with. Type /help for usage!"
 
-@bot.command(name="run", help="Run cyan run!")
-async def run_literally(context: commands.Context):
+        await bot.change_presence(activity=Game(name="Cuddling Python"))
+        await bot.get_channel(args.channel_id).send(message)
 
-    await context.send("https://cdn.discordapp.com/attachments/783069235999014912/840531297499480084/ezgif.com-gif-maker.gif")
+    @bot.event
+    async def on_ready():
+        nonlocal first_setup_done
 
+        print(f"{bot.user} connected.")
 
-@bot.command(name="py", help="Execute python code in Docker(not yet)")
-async def run_script(context: commands.Context, *, code: str):
+        if not any(guild.id == args.guild_id for guild in bot.guilds):
+            logger.critical("Bot is not connected to given server ID {}", args.guild_id)
 
-    # check code safety in future
-    # Extract code
-    striped = code.strip()
+            raise DiscordException(
+                f"Bot is not connected to given server ID {args.guild_id}"
+            )
 
-    if striped.startswith("```python"):
-        code = striped.removeprefix("```python").removesuffix("```")
+        if not first_setup_done:
+            first_setup_done = True
 
-    else:
-        await context.reply("Please use ```python\n<code>\n``` format!")
+            await first_call()
 
-    # Dumb way when it's not certain - put sequence of try-except, lel
+    # --------------------------------------
 
-    code_ = code_format.format(code)
+    linux_info_command = (
+        """/etc/update-motd.d/00-header && /etc/update-motd.d/90-updates-available"""
+    )
 
-    logger.info("Received code from %s by %s - detail: %s", context.channel, context.author, code_)
+    shell_field_commands = {
+        "CPU / RAM": '''echo "CPU `LC_ALL=C top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}'`% RAM `free -m | awk '/Mem:/ { printf("%3.1f%%", $3/$2*100) }'`"''',
+        "CPU Model": """cat /proc/cpuinfo | awk -F : '/model name/ {print $2}' | head -1 | xargs""",
+        "Disk usage": """df -h | grep -e /dev/sd -e md""",
+    }
 
-    try:
-        proc = await asyncio.create_subprocess_shell(code_, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
-    except Exception as err_:
-        await context.reply(msg_format.format(err_))
-        return
+    @bot.command(name="sysinfo", help="Get general system info. But why?")
+    async def system_information(context: commands.Context):
 
-    try:
-        stdout, stderr = await proc.communicate()
-    except Exception as err_:
-        await context.reply(f"{msg_format.format(err_)}\n\nExited with return code {proc.returncode}")
-        return
+        logger.debug("Call on sysinfo.")
 
-    output = []
+        description_proc = await asyncio.create_subprocess_shell(
+            linux_info_command,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+        stdout, _ = await description_proc.communicate()
 
-    if stdout:
-        output.append(stdout.decode(codec))
+        description = stdout.decode(codec)
 
-    if stderr:
-        output.append(stderr.decode(codec))
+        logger.debug(description)
 
-    message = "\n".join(output)
+        embed = Embed(title="Brief System information", description=description)
 
-    exit_msg = f"\nExited with return code {proc.returncode}"
+        for key, val in shell_field_commands.items():
+            proc = await asyncio.create_subprocess_shell(
+                val, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+            )
+            stdout, _ = await proc.communicate()
 
-    try:
-        await context.reply(msg_format.format(message) + exit_msg)
-    except HTTPException as err_:
+            logger.debug(dec := stdout.decode(codec))
 
-        if len(message) + len(exit_msg) + (len(msg_format) - 2) >= 2000:
-            fitted = message[:2000 - len(overflow_msg) - len(exit_msg) - (len(msg_format) - 2)]
-            cut_target = fitted.split("\n")[-1]
-            message = fitted.removesuffix(cut_target) + overflow_msg
+            embed.add_field(name=key, value=dec, inline=False)
 
-            await context.reply(message + exit_msg)
+        await context.reply(embed=embed)
+
+    # --------------------------------------
+
+    @bot.command(name="run", help="Run cyan run!")
+    async def run_literally(context: commands.Context):
+
+        logger.debug("Call on run")
+
+        await context.send(
+            "https://cdn.discordapp.com/attachments/783069235999014912/840531297499480084/ezgif.com-gif-maker.gif"
+        )
+
+    # --------------------------------------
+
+    codec = "utf8"
+
+    @bot.command(name="py", help="Execute python code in Docker(not yet)")
+    async def run_script(context: commands.Context, *, code: str):
+
+        # Extract code
+        striped = code.strip()
+
+        if striped.startswith("```python"):
+            code = striped.removeprefix("```python").removesuffix("```")
+
+        else:
+            await context.reply("Please use ```python\n<code>\n``` format!")
+
+        # Dumb way when it's not certain - put sequence of try-except, lel
+
+        # code_ = code_format.format(code)
+        code_ = code
+
+        logger.info(
+            "Received code from {} by {}\ndetail: {}",
+            context.channel,
+            context.author,
+            code_,
+        )
+
+        if not args.ip or not args.port:
+            await context.reply(
+                "Sorry! My owner didn't provide me either IP or Port, I can't access server for execution!"
+            )
             return
 
-        # This shouldn't run
-        logger.debug("Got other http error: %s", err_)
+        try:
+            reader, writer = await asyncio.open_connection(args.ip, port=args.port)
+        except Exception as err_:
+            logger.critical(err_)
+            logger.critical(f"IP was {args.ip}, and port was {args.port}")
+
+            await context.reply(
+                f"Encountered error on my side! It's not your fault!\n\n{err_}"
+            )
+            return
+
+        # Send code
+        send_byte = encode(code_)
+        writer.write(send_byte)
+        await writer.drain()
+        logger.info("Sent {}", len(send_byte))
+
+        # read data until delim is received
+        data = b""
+
+        try:
+            while end_signature_encoded not in data:
+                data += await asyncio.wait_for(reader.read(1024), timeout=10)
+        except asyncio.TimeoutError:
+            await context.reply("Got timeout error with your request! (< 10s)")
+            return
+
+        # decode and send it
+        resp = decode(data)
+        logger.debug("Got response, size {}", len(data))
+        await context.reply(resp)
+
+    # --------------------------------------
 
 
-if __name__ == '__main__':
-
-    logger = logging.getLogger("Meow.py")
+if __name__ == "__main__":
 
     # Parsing start
 
@@ -122,12 +193,28 @@ if __name__ == '__main__':
 
     parser.add_argument("bot_token", type=str, help="Bot's token")
     parser.add_argument("guild_id", type=int, help="Server's ID")
+    parser.add_argument("channel_id", type=int, help="Channel's ID")
+    parser.add_argument(
+        "ip",
+        type=str,
+        default="",
+        help="asyncio server's ip, if this is omitted you can't use python command.",
+    )
+    parser.add_argument(
+        "port",
+        type=int,
+        default=0,
+        help="asyncio server's port, if this is omitted you can't use python command.",
+    )
 
     args = parser.parse_args()
 
-    init_logger(logger, True)
+    bot_ = commands.Bot(
+        command_prefix="/", description="Bot for cyan's robot playground!"
+    )
+    assign_actions(bot_)
 
     try:
-        bot.run(args.bot_token)
+        bot_.run(args.bot_token)
     except DiscordException as err:
-        logger.critical("DiscordException - is token valid? Details: %s", err)
+        logger.critical("DiscordException - is token valid? Details: {}", err)
