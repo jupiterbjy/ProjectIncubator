acb1e81097f7646b23be5e3353eaf8d5ae9d9b12
jupiterbjy <jupiterbjy@gmail.com>
2021-08-12 21:53:02 +0900

    [squarefy_image.py] Added new script for batch square-fying images.

---
+++ single_run_scripts/squarefy_image.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import pathlib
+import argparse
+import itertools
+import time
+import math
+from typing import List
+
+from PIL import Image
+
+
+def main(image_files_: list[pathlib.Path]):
+
+    root = pathlib.Path(__file__).parent.joinpath("Squared")
+    root.mkdir(exist_ok=True)
+
+    for image_path in image_files_:
+        image: Image.Image = Image.open(image_path).convert("RGBA")
+
+        img_x, img_y = image.size
+        dim = max(img_x, img_y)
+
+        x_offset = (dim - img_x) // 2
+        y_offset = (dim - img_y) // 2
+
+        # create new image
+        new_empty = Image.new("RGBA", (dim, dim))
+
+        # paste image
+        new_empty.paste(image, (x_offset, y_offset))
+
+        # save
+        new_empty.save(root.joinpath(image_path.name))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("images", metavar="I", type=pathlib.Path, nargs="+", help="Images to horizontally merge with.")
+
+    args = parser.parse_args()
+
+    image_files: list = args.images
+    image_files.sort(key=lambda x: x.stem)
+
+    try:
+        main(image_files)
+    except Exception:
+        import traceback
+        traceback.print_exc()
+        input()
