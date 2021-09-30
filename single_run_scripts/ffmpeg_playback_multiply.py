81ea87f927cda105ff55aa9504bd1d9ea7318bcd
jupiterbjy <jupiterbjy@gmail.com>
2021-09-30 22:05:32 +0900

    Minor bug fixes & new script

---
+++ single_run_scripts/ffmpeg_playback_multiply.py
@@ -0,0 +1,34 @@
+import pathlib
+import asyncio
+import argparse
+from typing import List
+
+
+FPS = 60
+SPEED_MULTIPLIER = float(input("Video speed multiplier: "))
+
+FRAME_TIME = round(1 / SPEED_MULTIPLIER, 4)
+FFMPEG_CMD = "ffmpeg -y -i {} -r 60 -filter:v \"setpts={}*PTS\" {}"
+
+
+async def main():
+    file_list: List[pathlib.Path] = args.VIDEO
+
+    new_file_list = [file.with_name(file.stem + f"_x{SPEED_MULTIPLIER}" + file.suffix) for file in file_list]
+
+    for file, new_file in zip(file_list, new_file_list):
+        formatted = FFMPEG_CMD.format(file.as_posix(), FRAME_TIME, new_file.as_posix())
+
+        proc = await asyncio.create_subprocess_shell(formatted, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.STDOUT)
+
+        while line := await proc.stdout.readline():
+            print(line.decode(), end="")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("VIDEO", metavar="VID", type=pathlib.Path, nargs="+")
+
+    args = parser.parse_args()
+
+    asyncio.run(main())
---
+++ single_run_scripts/twitchPoint.py
@@ -0,0 +1,24 @@
+import time
+
+from selenium import webdriver
+
+
+xpath_form = "//{type}[@{attribute}='{attribute_value}']"
+
+
+def routine(driver, input_xform: str):
+    while True:
+        time.sleep(1)
+        element = driver.find_elements_by_xpath(input_xform)
+        if element:
+            print("Found element")
+            element[0].click()
+
+
+def main():
+    driver = webdriver.Chrome()
+    routine(driver, xpath_form.format_map({"type": "button", "attribute": "aria-label", "attribute_value": "보너스 받기"}))
+
+
+if __name__ == '__main__':
+    main()
