369d2fe716e154e1b7a3317da9109813f08965be
jupiterbjy <jupiterbjy@gmail.com>
2022-03-10 16:40:29 +0900

    Added SO answer

---
+++ SO/71416383-python-asyncio-cancelling-a-to-thread-task-wont-stop-the-thread/demo.py
@@ -0,0 +1,27 @@
+"""
+Demo codes for https://stackoverflow.com/questions/71416383/
+
+Showing canceling(not really) task running in thread
+
+Python nor OS provide general mechanism to stop an arbitrary thread running synchronous code
+"""
+
+import time
+import asyncio
+
+
+def blocking_func(event: asyncio.Event):
+    while not event.is_set():
+        time.sleep(1)
+        print("I'm still standing")
+
+
+async def main():
+    event = asyncio.Event()
+    asyncio.create_task(asyncio.to_thread(blocking_func, event))
+
+    await asyncio.sleep(5)
+    # now lets stop
+    event.set()
+
+asyncio.run(main())
