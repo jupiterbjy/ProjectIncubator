9670121ff0256a3b9f265f863ea7f1b18ec686a6
jupiterbjy <jupiterbjy@gmail.com>
2022-05-29 22:16:55 +0900

    72421640 solution code update

--- SO/72421640-callback-when-tasks-are-added-removed-in-trio-nursery/solution.py
+++ SO/72421640-callback-when-tasks-are-added-removed-in-trio-nursery/solution.py
@@ -1,8 +1,10 @@
 """
 Based on https://stackoverflow.com/a/72422377/10909029
+
+For further information head out to:
+https://trio.readthedocs.io/en/stable/reference-lowlevel.html#instrument-api
 """
 
-import time
 import random
 from collections import defaultdict
 
@@ -10,21 +12,29 @@ import trio
 from trio.lowlevel import add_instrument
 from trio.abc import Instrument
 
-task_counts = defaultdict(lambda: 0)
-
 
 class TaskCountInstrument(Instrument):
+    """
+    Task counting instrument
+    """
+    def __init__(self):
+        self.task_counts = defaultdict(lambda: 0)
+
     def task_spawned(self, task):
-        task_counts[task.parent_nursery] += 1
-        print(f"[N:{id(task.parent_nursery)}][R:{task_counts[task.parent_nursery]}] task {id(task)} spawned!")
+        parent = task.parent_nursery
+
+        self.task_counts[parent] += 1
+        print(f"[N:{id(parent)}][R:{self.task_counts[parent]}] task {id(task)} spawned!")
 
     def task_exited(self, task):
-        if task.parent_nursery not in task_counts:
+        parent = task.parent_nursery
+
+        if parent not in self.task_counts:
             # if it's not there probably not my fault, must be internal nurseries.
             return
 
-        task_counts[task.parent_nursery] -= 1
-        print(f"[N:{id(task.parent_nursery)}][R:{task_counts[task.parent_nursery]}] task {id(task)} finished!")
+        self.task_counts[parent] -= 1
+        print(f"[N:{id(parent)}][R:{self.task_counts[parent]}] task {id(task)} finished!")
 
 
 async def dummy_task(task_no, lifetime):
@@ -36,13 +46,14 @@ async def dummy_task(task_no, lifetime):
         lifetime: Lifetime of task
     """
     print(f"  Task {task_no} started")
-    start = time.time()
     await trio.sleep(lifetime)
     print(f"  Task {task_no} finished")
 
 
 async def main():
+    # Adding instrument
     add_instrument(TaskCountInstrument())
+
     async with trio.open_nursery() as nursery:
         for n in range(5):
             nursery.start_soon(dummy_task, n, random.randint(1, 4))
