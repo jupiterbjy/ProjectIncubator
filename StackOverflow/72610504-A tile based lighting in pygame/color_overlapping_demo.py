75f08df62f4f91b1d966ee3b4f7d179012b4b233
jupiterbjy <jupiterbjy@gmail.com>
2022-06-16 01:02:37 +0900

    Updated 72610504-A

---
+++ StackOverflow/72610504-A tile based lighting in pygame/color_overlapping_demo.py
@@ -0,0 +1,62 @@
+"""
+Demonstration of color overlapping
+"""
+
+import pygame as pg
+
+
+class Player(pg.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.image = pg.Surface((50, 50))
+        self.image.fill((255, 255, 255))
+        self.rect = self.image.get_rect()
+
+        # setting alpha on player
+        self.image.set_alpha(125)
+
+    def update(self, *args, **kwargs):
+        x, y = pg.mouse.get_pos()
+        c_x, c_y = self.rect.center
+        self.rect.move_ip(x - c_x, y - c_y)
+
+
+def mainloop():
+    player = Player()
+    screen = pg.display.set_mode((500, 500))
+
+    circle_colors = (255, 0, 0), (0, 255, 0), (0, 0, 255)
+    circle_coords = (150, 250), (250, 250), (350, 250)
+
+    # make surface, set alpha then draw circle
+    bg_surfaces = []
+    for (color, center) in zip(circle_colors, circle_coords):
+        surface = pg.Surface((500, 500), pg.SRCALPHA, 32)
+        surface.convert_alpha()
+        surface.set_alpha(125)
+        pg.draw.circle(surface, color, center, 75)
+        bg_surfaces.append(surface)
+
+    running = True
+
+    while running:
+        screen.fill((0, 0, 0))
+
+        for surface in bg_surfaces:
+            screen.blit(surface, surface.get_rect())
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+
+        # draw transparent circles
+
+        player.update()
+        screen.blit(player.image, player.rect)
+        pg.display.flip()
+
+
+if __name__ == '__main__':
+    pg.init()
+    mainloop()
+    pg.quit()
--- StackOverflow/72610504-A tile based lighting in pygame/pygame_tile_based_lighting.py
+++ StackOverflow/72610504-A tile based lighting in pygame/pygame_tile_based_lighting.py
@@ -1,13 +1,14 @@
+"""
+Code demonstration for https://stackoverflow.com/q/72610504/10909029
+Written on Python 3.10 (Using Match on input / event dispatching)
+"""
+
 import math
 import random
+import itertools
 from typing import Dict, Tuple, Sequence
 
-import pygame
 import pygame as pg
-
-
-pg.init()
-pg.font.init()
 
 
 class Position:
@@ -17,9 +18,30 @@ class Position:
 
 
 class SpriteGroup:
-    """Namespace for sprite groups"""
+    """Namespace for sprite groups, with chain iterator keeping the order"""
     ground = pg.sprite.Group()
+    entities = pg.sprite.Group()
     light_overlay = pg.sprite.Group()
+
+    @classmethod
+    def all_sprites(cls):
+        return itertools.chain(cls.ground, cls.entities, cls.light_overlay)
+
+
+class Player(pg.sprite.Sprite):
+    """Player class, which is merely a rect following pointer in this example."""
+    def __init__(self):
+        super(Player, self).__init__()
+        self.image = pg.Surface((25, 25))
+        self.image.fill((255, 255, 255))
+        self.rect = self.image.get_rect()
+
+        SpriteGroup.entities.add(self)
+
+    def update(self, *args, **kwargs):
+        x, y = pg.mouse.get_pos()
+        c_x, c_y = self.rect.center
+        self.rect.move_ip(x - c_x, y - c_y)
 
 
 class TileLightOverlay(pg.sprite.Sprite):
@@ -50,7 +72,7 @@ class TileLightOverlay(pg.sprite.Sprite):
 
     @property
     def brightness(self):
-        """Calculate distance between mouse"""
+        """Calculate distance between mouse & apply light falloff accordingly"""
         distance = math.dist(self.rect.center, pg.mouse.get_pos())
 
         if distance > self.light_radius:
@@ -60,6 +82,8 @@ class TileLightOverlay(pg.sprite.Sprite):
 
 
 class TileGround(pg.sprite.Sprite):
+    """Ground tile representation. Not much is going on here."""
+
     def __init__(self, x, y, tile_color: Sequence[float]):
         super(TileGround, self).__init__()
 
@@ -71,24 +95,26 @@ class TileGround(pg.sprite.Sprite):
 
         SpriteGroup.ground.add(self)
 
+        # create and keep its pair light overlay tile.
         self.light_tile = TileLightOverlay(x, y)
 
 
 class World:
+    """World storing ground tile data."""
     # tile type storing color etc. for this example only have color.
     tile_type: Dict[int, Tuple[float, float, float]] = {
-        0: (100, 100, 100),
-        1: (255, 255, 255),
+        0: (56, 135, 93),
+        1: (36, 135, 38),
+        2: (135, 128, 56)
     }
 
     def __init__(self):
         # coord system : +x → / +y ↓
         # generating random tile data
         self.tile_data = [
-            [random.randint(0, 1) for _ in range(25)]
+            [random.randint(0, 2) for _ in range(25)]
             for _ in range(25)
         ]
-
         # generated tiles
         self.tiles = []
 
@@ -103,65 +129,68 @@ def process_input(event: pg.event.Event):
     """Process input, in case you need it"""
     match event.key:
         case pg.K_ESCAPE:
-            pg.event.post(pygame.event.Event(pg.QUIT))
+            pg.event.post(pg.event.Event(pg.QUIT))
         case pg.K_UP:
             pass
         # etc..
 
 
-def display_fps_closure(screen: pg.Surface):
+def display_fps_closure(screen: pg.Surface, clock: pg.time.Clock):
+    """FPS display"""
     font_name = pg.font.get_default_font()
     font = pg.font.Font(font_name, 10)
     color = (0, 255, 0)
 
-    def inner(fps: float):
-        text = font.render(f"{int(fps)} fps", True, color)
+    def inner():
+        text = font.render(f"{int(clock.get_fps())} fps", True, color)
         screen.blit(text, text.get_rect())
 
     return inner
 
 
 def mainloop():
+    # keeping reference of method/functions to reduce access overhead
+    fetch_events = pg.event.get
     display = pg.display
+
+    # local variable setup
     screen = display.set_mode((500, 500))
-    screen.fill((0, 0, 0))
-
-    running = True
-    events = pg.event.get
-
+    player = Player()
     world = World()
     world.generate()
 
     clock = pg.time.Clock()
-    display_fps = display_fps_closure(screen)
+    display_fps = display_fps_closure(screen, clock)
+
+    running = True
 
     # main loop
     while running:
+        screen.fill((0, 0, 0))
+
         # process event
-        for event in events():
+        for event in fetch_events():
+            # event dispatch
             match event.type:
                 case pg.QUIT:
                     running = False
                 case pg.KEYDOWN:
                     process_input(event)
 
-        # draw ground tiles
-        for sprite in SpriteGroup.ground:
-            screen.blit(sprite.image, sprite.rect)
-
-        # draw light overlay tiles
-        for sprite in SpriteGroup.light_overlay:
+        # draw in ground > entities > light overlay order
+        for sprite in SpriteGroup.all_sprites():
             sprite.update()
             screen.blit(sprite.image, sprite.rect)
 
-        # draw fps
-        clock.tick(240)
-        display_fps(clock.get_fps())
+        # draw fps - not related to question, was lazy to remove & looks fancy
+        clock.tick()
+        display_fps()
 
         display.flip()
 
-    pg.quit()
-
 
 if __name__ == '__main__':
+    pg.init()
+    pg.font.init()
     mainloop()
+    pg.quit()
