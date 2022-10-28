b3e441b47f33bec4f055aa32e5eeae4d49644bf5
jupiterbjy <jupiterbjy@gmail.com>
2022-10-29 03:24:12 +0900

    SO answer update

---
+++ StackOverflow/74132406-A asyncio isort/import_messes/lib_0.py
@@ -0,0 +1,18 @@
+"""
+Asyncio __init__ file import section for mass import
+"""
+
+from asyncio.base_events import *
+from asyncio.coroutines import *
+from asyncio.events import *
+from asyncio.exceptions import *
+from asyncio.futures import *
+from asyncio.locks import *
+from asyncio.protocols import *
+from asyncio.runners import *
+from asyncio.queues import *
+from asyncio.streams import *
+from asyncio.subprocess import *
+from asyncio.tasks import *
+from asyncio.threads import *
+from asyncio.transports import *
