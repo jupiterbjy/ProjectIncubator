7b9d7738e66b735703017e574732f07faf7d7233
jupiterbjy <jupiterbjy@gmail.com>
2023-04-07 03:08:43 +0900

    [SingleScriptTools] Fixed ffmpeg_playback_multiply failing with file name containing whitespace

--- SingleScriptTools/ffmpeg_playback_multiply_▽.py
+++ SingleScriptTools/ffmpeg_playback_multiply_▽.py
@@ -13,24 +13,32 @@ FPS = 60
 SPEED_MULTIPLIER = float(input("Video speed multiplier: "))
 
 FRAME_TIME = round(1 / SPEED_MULTIPLIER, 4)
-FFMPEG_CMD = "ffmpeg -y -i {} -r 60 -filter:v \"setpts={}*PTS\" {}"
+FFMPEG_CMD = 'ffmpeg -y -i {} -r 60 -filter:v "setpts={}*PTS" {}'
 
 
 async def main():
     file_list: List[pathlib.Path] = args.VIDEO
+    print("Target(s):", file_list)
 
-    new_file_list = [file.with_name(file.stem + f"_x{SPEED_MULTIPLIER}" + file.suffix) for file in file_list]
+    new_file_list = [
+        file.with_name(file.stem + f"_x{SPEED_MULTIPLIER}" + file.suffix)
+        for file in file_list
+    ]
 
     for file, new_file in zip(file_list, new_file_list):
-        formatted = FFMPEG_CMD.format(file.as_posix(), FRAME_TIME, new_file.as_posix())
+        formatted = FFMPEG_CMD.format(
+            f'"{file.as_posix()}"', FRAME_TIME, f'"{new_file.as_posix()}"'
+        )
 
-        proc = await asyncio.create_subprocess_shell(formatted, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.STDOUT)
+        proc = await asyncio.create_subprocess_shell(
+            formatted, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.STDOUT
+        )
 
         while line := await proc.stdout.readline():
             print(line.decode(), end="")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("VIDEO", metavar="VID", type=pathlib.Path, nargs="+")
 
