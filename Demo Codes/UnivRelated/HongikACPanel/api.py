3023ac63ac7b3bc495de07491034f51d9142eaf3
jupiterbjy <jupiterbjy@gmail.com>
2023-05-01 06:19:16 +0900

    Overhauled HongikACPanel API to better match actual usecase.

--- Demo Codes/UnivRelated/HongikACPanel/api.py
+++ Demo Codes/UnivRelated/HongikACPanel/api.py
@@ -15,7 +15,7 @@ import re
 import functools
 import argparse
 from random import randint
-from typing import Dict
+from typing import Dict, Any
 
 import trio
 import httpx
@@ -25,15 +25,21 @@ from bs4 import BeautifulSoup as bs
 from loguru import logger
 
 
-URL = "http://ENTER_IP_HERE/"
-REMOTE_URL = URL + "webremo"
-
 # AC mode translation list
 OPERATION_MODE = ["Heating", "Cooling", "Fan only"]
 WIND_SPEED_MODE = ["Auto", "Max", "medium", "low"]
 WIND_DIRECTION_MODE = ["Swing", "Horizontal", "Vertical"]
 
 
+class ACTempOutOfBound(Exception):
+    pass
+
+
+class ACRequestFailed(Exception):
+    pass
+
+
+# TODO: Check if we need target temp here
 class ACState:
     """
     Simplified web remote status representation from Parsed HTML.
@@ -103,18 +109,14 @@ class ACState:
 
     @functools.cached_property
     def current_temp(self) -> int:
-        """
-        Gets current temperature.
-        """
+        """Gets current temperature."""
 
         img_src = self.soup.find("img", {"id": "Image_2"})["src"]
         return self._pattern_match(img_src)
 
     @functools.cached_property
     def target_temp(self) -> int:
-        """
-        Gets target temperature.
-        """
+        """Gets target temperature."""
 
         img_src = self.soup.find("img", {"id": "Image_3"})["src"]
         return self._pattern_match(img_src)
@@ -141,6 +143,7 @@ class ACManager:
     AC controller class.
     """
 
+    # TODO: validate effect of hdrNo12 & 13 and update temp_up & temp_down.
     """
     <MEMO>
     Guessed from html source & images
@@ -158,7 +161,7 @@ class ACManager:
     10 = 1
     11 = 1
     12 = 29 AC upper temp lim, must be < 29
-    13 = 25 AC lower temp lim, must be 
+    13 = 25 AC lower temp lim, must be > 25
     14 = 1
     15 = 1
     16 = 1
@@ -177,7 +180,7 @@ class ACManager:
         "off": (108, 40),
         "temp_down": (95, 35),
         "temp_up": (41, 37),
-        "": (55, 16),
+        "other": (55, 16),
     }
 
     # TODO: move base_state and associated to ACState
@@ -205,16 +208,44 @@ class ACManager:
         "btnSubmit.y": 16,
     }
 
-    def __init__(self, id_, password):
+    def __init__(self, ip, id_, password):
         self.client = httpx.AsyncClient()
 
-        self.id_ = id_
-        self.pw = password
+        self._url = f"http://{ip}/"
+        self._url_remote = self._url + "webremo"
 
+        self._id = id_
+        self._pw = password
+
+        # AC states to keep track of
         self.state: None | ACState = None
-        self.target_temp = 0
-        self.action = ""
-        self.power_on = False
+        self.target_temp = (self.upper_bound + self.lower_bound) // 2
+        self.power_state = False
+        self.action = "other"
+
+    @functools.cached_property
+    def upper_bound(self) -> int:
+        """Upper temp boundary"""
+
+        return self.base_state["hdnNo_12"]
+
+    @functools.cached_property
+    def lower_bound(self) -> int:
+        """Lower temp boundary"""
+
+        return self.base_state["hdnNo_13"]
+
+    @property
+    def payload(self) -> Dict[str, Any]:
+        """Creates payload."""
+
+        payload = {k: v for k, v in self.base_state.items()}
+        payload["hdnNo_4"] = self.target_temp
+        payload["hdnNo_1"] = 1 if self.power_state else 0
+
+        payload["btnSubmit.x"], payload["btnSubmit.y"] = self.btn_action[self.action]
+
+        return self.state.states | payload
 
     async def login(self):
         """
@@ -226,109 +257,189 @@ class ACManager:
 
         # initial update
         if self.state is None:
-            resp = await self.client.get(URL, follow_redirects=True)
+            resp = await self.client.get(self._url, follow_redirects=True)
             self.state = ACState(resp)
 
         payloads = self.state.states
 
         # adding login data
-        payloads["txtId"] = self.id_
-        payloads["txtPwd"] = self.pw
+        payloads["txtId"] = self._id
+        payloads["txtPwd"] = self._pw
 
         # proceed login & update state
-        resp = await self.client.post(URL, data=payloads, follow_redirects=True)
+        resp = await self.client.post(self._url, data=payloads, follow_redirects=True)
 
         resp.raise_for_status()
         logger.info(f"Login successful")
 
         await self.update(resp)
 
-    async def update(self, resp: httpx.Response = None):
-        """
-        Manually trigger update.
-        """
+    async def update(self, resp: httpx.Response = None) -> ACState:
+        """Manually trigger update & returns state."""
 
         if resp is None:
-            resp = await self.client.get(URL, follow_redirects=True)
+            resp = await self.client.get(self._url, follow_redirects=True)
 
         self.state = ACState(resp)
         self.target_temp = self.state.target_temp
+
         logger.info(f"Cur. Temp      : {self.state.current_temp}")
         logger.info(f"Cur. Operation : {self.state.operation_mode}")
         logger.info(f"Cur. Wind speed: {self.state.wind_speed}")
         logger.info(f"Cur. Wind angle: {self.state.wind_angle}")
 
-    async def keep_alive(self, interval_sec=600, deviation_max_sec=60):
+        return self.state
+
+    async def keep_alive_state(self, interval_sec=60, deviation_max_sec=30):
+        """Keep state up-to-date"""
+
+        logger.debug("Keepalive State started")
+
+        while True:
+            await trio.sleep(interval_sec + randint(0, deviation_max_sec))
+            await self.update()
+
+    async def keep_alive_power(self, interval_sec=1200, deviation_max_sec=60):
         """
         Re-login and send commands again with given interval.
         Randomly adds up to deviation_max_sec to total waiting time.
+
+        Expects keep_alive_state to be run alongside.
         """
-        logger.debug("Keepalive started")
+
+        logger.debug("Keepalive Power started")
 
         while True:
             sleep_duration = interval_sec + randint(0, deviation_max_sec)
-            logger.info(f"Sleeping for {sleep_duration}")
+            logger.debug(f"Sleeping for {sleep_duration}")
 
             await trio.sleep(sleep_duration)
-            await self.login()
-            await self.send(True, self.target_temp)
+            await self._send()
 
-    @property
-    def payload(self):
+    async def get_temp(self):
+        """Run update and get new current temp."""
+
+        await self.update()
+        return self.state.current_temp
+
+    async def power_on(self):
+        """Power On AC.
+
+        Raises:
+            ACRequestFailed: If request was failed
         """
-        Creates payload.
+
+        self.action = "on"
+        self.power_state = True
+        await self._send()
+
+    async def power_off(self):
+        """Power Off AC.
+
+        Raises:
+            ACRequestFailed: If request was failed
         """
-        payload = {k: v for k, v in self.base_state.items()}
-        payload["hdnNo_4"] = self.target_temp
-        payload["hdnNo_1"] = 1 if self.power_on else 0
 
-        payload["btnSubmit.x"], payload["btnSubmit.y"] = self.btn_action[self.action]
+        self.action = "off"
+        self.power_state = False
+        await self._send()
 
-        return self.state.states | payload
+    async def set_temp(self, temp: int):
+        """Set specific temp.
 
-    async def send(self, power: bool, temp: int):
+        Args:
+            temp: Target Temperature
+
+        Raises:
+            ACTempOutOfBound: If set temperature is out of bound
+            ACRequestFailed: If request was failed
         """
-        Sends request.
+
+        if not(self.upper_bound < temp < self.lower_bound):
+            raise ACTempOutOfBound()
+
+        self.target_temp = temp
+        await self._send()
+
+    async def temp_down(self):
+        """Lower temp by 1.
+
+        Raises:
+            ACTempOutOfBound: If set temperature is out of bound
+            ACRequestFailed: If request was failed
+        """
+
+        if (self.target_temp - 1) <= self.lower_bound:
+            raise ACTempOutOfBound()
+
+        self.target_temp -= 1
+        await self._send()
+
+    async def temp_up(self):
+        """Higher temp by 1.
+
+        Raises:
+            ACTempOutOfBound: If set temperature is out of bound
+            ACRequestFailed: If request was failed
+        """
+
+        if (self.target_temp + 1) >= self.upper_bound:
+            raise ACTempOutOfBound()
+
+        self.target_temp += 1
+        await self._send()
+
+    async def _send(self):
+        """Sends request.
+
+        Raises:
+            ACRequestFailed: If request operation fails
         """
 
         # TODO: add & raise custom errors (i.e. permission, out of temp range, etc)
-
-        self.power_on = power
-        self.target_temp = temp
+        # TODO: Check all possible exceptions from Univ's AC web remote server
 
         logger.info("Sending request!")
-        logger.debug(f"Power {self.power_on} / TGT Temp {self.target_temp}")
+        logger.debug(f"Power {self.power_state} / TGT Temp {self.target_temp}")
 
-        resp = await self.client.post(REMOTE_URL, data=self.payload, follow_redirects=True)
+        resp = await self.client.post(self._url_remote, data=self.payload, follow_redirects=True)
 
         try:
             resp.raise_for_status()
+
         except Exception as err:
             logger.warning(f"{type(err).__name__} - {err}")
             logger.debug(f"Received response:\n{resp.content.decode()}\n")
+            raise ACRequestFailed() from err
 
-        # await self.update()
+        finally:
+            # reset action
+            self.action = "other"
 
 
 async def main(arguments):
     logger.info("Note: This script will automatically stop AC when shutting down.")
 
-    ac = ACManager(arguments.id, arguments.pwd)
+    ac = ACManager(arguments.ip, arguments.id, arguments.pwd)
 
     await ac.login()
-    await ac.send(True, arguments.temp)
+    await ac.power_on()
+    await ac.set_temp(arguments.temp)
 
-    try:
-        await ac.keep_alive()
+    async with trio.open_nursery() as nursery:
+        nursery.start_soon(ac.keep_alive_power())
+        nursery.start_soon(ac.keep_alive_state())
 
-    except KeyboardInterrupt:
-        await ac.send(False, arguments.temp)
-        logger.info("Shutting down!")
+    await ac.power_off()
+    logger.info("Shutting down!")
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("Controls AC")
-
+    parser.add_argument(
+        type=str,
+        help="Web remote IP"
+    )
     parser.add_argument(
         "-t",
         "--temp",
