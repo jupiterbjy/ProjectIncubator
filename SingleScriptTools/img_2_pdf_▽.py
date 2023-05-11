afc8ddafbde870b2ed2e0b9fda2b737887986ad9
jupiterbjy <jupiterbjy@gmail.com>
2023-05-12 03:19:20 +0900

    [img_2_pdf] Added docs; Added timeout before closing in case of error.

--- SingleScriptTools/img_2_pdf_▽.py
+++ SingleScriptTools/img_2_pdf_▽.py
@@ -4,35 +4,66 @@ Convert multiples images into single pdf
 
 import pathlib
 import argparse
+import time
+import traceback
 from typing import Sequence, Generator
 
 from PIL import Image
 
 
-def path_to_image_gen(paths: Sequence[pathlib.Path]) -> Generator[None, Image.Image, None]:
+TIMEOUT = 10
+
+
+def fetch_image_gen(paths: Sequence[pathlib.Path]) -> Generator[Image.Image, None, None]:
+    """Fetch images from given Sequence of Path objects.
+
+    Args:
+        paths: Sequence of image paths
+
+    Returns:
+        Generator yielding PIL Images
+    """
+
     for path in paths:
+        print(f"Loading {path.name}")
         img_temp = Image.open(path)
 
-        # convert all into RGB
-        if img_temp.mode != "RGB":
-            img_temp = img_temp.convert("RGB")
+        # convert all into RGB, no RGBA or else allowed
+        if img_temp.mode == "RGBA":
+
+            # use white bg
+            bg = Image.new("RGBA", img_temp.size, (255, 255, 255))
+            bg.alpha_composite(img_temp)
+            img_temp = bg
+
+        elif img_temp.mode != "RGB":
+            img_temp.convert("RGB")
 
         yield img_temp
 
 
 def image_path_to_pdf(paths: Sequence[pathlib.Path]):
+    """Load given image paths and save as pdf file.
+
+    Args:
+        paths: Sequence of image paths
+    """
     save_file = paths[0].with_suffix(".pdf")
-    try:
-        first_img, *rest_img = path_to_image_gen(paths)
-    except Exception:
-        print(f"Encountered error while loading images. Check last traceback.")
-        raise
 
     try:
-        first_img: Image.Image
+        # fetch first img & append others
+        first_img, *rest_img = fetch_image_gen(paths)
         first_img.save(save_file, "pdf", save_all=True, append_images=rest_img)
-    except Exception:
-        print(f"Encountered error while saving images. Check last traceback.")
+
+    except Exception as err:
+        # report error and wait
+
+        traceback.print_exc()
+
+        print(f"{type(err).__name__} while loading images. Check last traceback.")
+        print(f"Will be closed in {TIMEOUT} seconds.")
+        time.sleep(TIMEOUT)
+
         raise
 
 
@@ -40,6 +71,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         "Img2PDF",
         description="Combine images into single pdf file."
+                    "Will use first file name as resulting PDF."
     )
 
     parser.add_argument(
